610ee2d7eae13c1605e6838ef3b92a3741c9a1c4
prasadbobby <prasadbobby057@gmail.com>
2020-07-09 20:00:50 +0530

    Updates

---
+++ precache-manifest.dbcbfa8ad45feb77d65b9b5696c33508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc1cb18b000e24ea6dcf3d17e54ea03",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "19906eacb05d3c419ada",
+    "url": "/react-task/static/css/main.d4066631.chunk.css"
+  },
+  {
+    "revision": "94bf43eba012d02c97bb",
+    "url": "/react-task/static/js/2.844e7671.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-task/static/js/2.844e7671.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19906eacb05d3c419ada",
+    "url": "/react-task/static/js/main.ceeead9a.chunk.js"
+  },
+  {
+    "revision": "7a56f723fc9bbcd51127",
+    "url": "/react-task/static/js/runtime-main.8f0e6c8e.js"
+  }
+]);
